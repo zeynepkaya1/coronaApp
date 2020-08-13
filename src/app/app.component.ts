3d5d978d39fedab7575f6ad2ef3056874bb355c5
Zeynep Kaya <1998.ze.ka@gmail.com>
2020-08-14 00:57:15 +0300

    Basic HTML

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Model}  from './model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
-  title = 'coronaApp';
+  model = new Model();
+
+  getItems() {
+    return this.model.items;
+  }
+
 }
